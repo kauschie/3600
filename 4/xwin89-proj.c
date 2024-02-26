52e9615eb3ba5e2bcd2ab9161fbdbd2f85153dd4
Michael Kausch <mkausch@odin.cs.csubak.edu>
2024-02-25 17:01:31 -0800

    fixed proj and now bug free

--- 4/xwin89-proj.c
+++ 4/xwin89-proj.c
@@ -20,6 +20,7 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <pthread.h> // for threads
 
 
 //#define DEBUG
@@ -27,7 +28,7 @@
 //#define USE_SEM
 #define USE_SHM
 
-enum CollisionType { LR, UP };
+enum CollisionType { LEFT=1, RIGHT=2, TOP=3, BOTTOM=4};
 enum SharedStatus { NONE=1, CHILD=2, PARENT=3, BOTH=4 };
 
 struct Global {
@@ -50,6 +51,8 @@ struct Global {
     //enum SharedStatus *hasBall; // shared between windows
     struct sembuf grab[2], release[1];
     struct Shared * shr;
+    int runThread;
+    pthread_t tid;
 } g;
 
 struct Vec2 {
@@ -57,12 +60,13 @@ struct Vec2 {
     int y;
 };
 
+/*
+
 struct WinInfo {
     struct Vec2 dir;// velocity
     struct Vec2 boxScreen; // screen coords of box
     enum CollisionType ct; // left/right or up/down collision
 };
-/*
 
 struct WinMsg {
     long type;
@@ -74,8 +78,17 @@ struct Shared {
     struct Vec2 dir;
     struct Vec2 pos;
     struct Vec2 dim;
-    enum CollisionType ct;
+    struct Vec2 newPos;
+    //enum CollisionType ct;
     enum SharedStatus hasBall;
+    struct Vec2 pWinPos, cWinPos;
+
+    /*
+	Display *parent_dpy, *child_dpy;
+	Window parent_win, child_win;
+	GC parent_gc, child_gc;
+    
+    */
 };
 
 union {
@@ -96,7 +109,7 @@ void render(void);
 void physics(void);
 int check_winner(void);
 void x11_setFont(unsigned int idx);
-void getWindowCoords(int *x, int *y);
+void* getWindowCoords(void *);
 void setupMQ(void);
 void teardownMQ(void);
 void setupSEM(void);
@@ -107,6 +120,7 @@ void checkMSG(void);
 struct Vec2 translateCoords(struct Vec2 c);
 int checkLRCollision(void); 
 int checkUDCollision(void); 
+int checkExternalCollision(enum CollisionType ct);
 
 int myargc;
 char **myargv;
@@ -161,6 +175,9 @@ int main(int argc, char *argv[], char *envp[])
         }
 		usleep(4000);
     }
+
+    void* t_status;
+    pthread_join(g.tid, &t_status);
 	x11_cleanup_xwindows();
 
 #ifdef USE_MSQ
@@ -173,19 +190,20 @@ int main(int argc, char *argv[], char *envp[])
 
 #ifdef USE_SHM
     teardownSHM();
-    int status;
-    wait(&status);
+    int p_status;
+    wait(&p_status);
     shmctl(g.shmid, IPC_RMID, 0);
 #endif
 
 	return 0;
 }
 
+/*
 struct Vec2 translateCoords(struct Vec2 c)
 {
     struct Vec2 out_coords;
     int x,y;
-    getWindowCoords(&x,&y);
+    //getWindowCoords(g.dpy, &g.win, &x,&y);
 
     
     //out_coords.x = c.x-x;
@@ -194,6 +212,7 @@ struct Vec2 translateCoords(struct Vec2 c)
     out_coords.y = y-c.y;
     return out_coords;
 }
+*/
 
 #ifdef USE_MSQ
 void checkMSG(void)
@@ -216,7 +235,7 @@ void checkMSG(void)
             g.by = mymsg.w.boxScreen.y;
             printf("NEW box.x: %d box.y: %d dir.x: %d dir.y: %d\n",
                         g.bx, g.by, g.xdirection, g.ydirection);
-            printf("press enter to continue...\n");
+            //printf("press enter to continue...\n");
             //getchar();
             return; // leave without touching other queues
         }
@@ -238,7 +257,7 @@ void checkMSG(void)
             g.by = mymsg.w.boxScreen.y;
             printf("NEW box.x: %d box.y: %d dir.x: %d dir.y: %d\n",
                         g.bx, g.by, g.xdirection, g.ydirection);
-            printf("press enter to continue...\n");
+            //printf("press enter to continue...\n");
             //getchar();
             return;
         }
@@ -318,7 +337,7 @@ void setupSEM(void)
     g.release[0].sem_num = 0;
     g.release[0].sem_flg = SEM_UNDO;
     g.release[0].sem_op = -1;
-    my_semun.val = 1;
+    my_semun.val = 0;
     semctl(g.semid, 0, SETVAL, my_semun);
 }
 void teardownSEM(void)
@@ -350,6 +369,17 @@ void setupSHM(void)
     g.shr->pos.x = rand() % (g.xres-g.shr->dim.x);
     g.shr->pos.y = rand() % (g.yres-g.shr->dim.y);
     //g.
+
+    
+    if (g.hasSpawned == 0 || g.cpid > 0) {
+        printf("creating thread for parent\n");
+
+    } else {
+        printf("creating thread for child\n");
+    }
+    
+    g.runThread = 1;
+    pthread_create(&g.tid, NULL, (void*)getWindowCoords, NULL);
 
 }
 
@@ -514,9 +544,13 @@ int check_keys(XEvent *e)
                     // parent
                     g.shr->hasBall = PARENT;
                 }
+                g.shr->pos.x = g.xres/2;
+                g.shr->pos.y = g.yres/2;
+                g.winner = 0;
 
                 break;
 			case XK_Escape:
+                g.runThread = 0;
 				return 1;
 
             case XK_c:
@@ -543,9 +577,10 @@ void render(void)
     char buf3[10];
     char buf4[128];
     char buf5[128];
+    char buf6[128];
     int color1 = 0x00ffc72c;
     int color2 = 0x00003594;
-    int wx =0, wy=0;
+    //int wx =0, wy=0;
     
     if (g.hasSpawned == 0 || g.cpid > 0) {
         // parent or hasn't spawned a child
@@ -556,6 +591,9 @@ void render(void)
             strcpy(buf3, "Parent");
             g.cur_color = color1;
             //sprintf(buf2, "mouse[x]: %d | mouse[y]: %d", g.mx, g.my);
+            sprintf(buf4, "window[x]: %d | window[y]: %d", 
+                                g.shr->pWinPos.x, g.shr->pWinPos.y);
+            sprintf(buf5, "hasBall: %d", (g.shr->hasBall == CHILD ? 1 : 0));
 
     } else if (g.cpid == 0) {
         // child
@@ -564,17 +602,12 @@ void render(void)
             strcpy(buf2, "Press 'esc' to close");
             strcpy(buf3, "Child");
             g.cur_color = color2;
+            sprintf(buf4, "window[x]: %d | window[y]: %d", 
+                                g.shr->cWinPos.x, g.shr->cWinPos.y);
+            sprintf(buf5, "hasBall: %d", (g.shr->hasBall == PARENT ? 1 : 0));
 
     }
 
-    //getWindowCoords(&wx, &wy);
-    sprintf(buf4, "window[x]: %d | window[y]: %d", wx, wy);
-    if (g.cpid == 0) {
-        sprintf(buf5, "hasBall: %d", (g.shr->hasBall == CHILD ? 1 : 0));
-    } else {
-        sprintf(buf5, "hasBall: %d", (g.shr->hasBall == PARENT ? 1 : 0));
-    }
-    
     XSetForeground(g.dpy, g.gc, g.cur_color); 
     XFillRectangle(g.dpy, g.win, g.gc, 0, 0, g.xres, g.yres);
 
@@ -603,15 +636,22 @@ void render(void)
         // draw box tex
         XSetForeground(g.dpy, g.gc, g.cur_color);
         XDrawString(g.dpy, g.win, g.gc, (g.shr->pos.x + (g.shr->dim.x/2) - 24), (g.shr->pos.y + (g.shr->dim.y/2) + 5), buf3, strlen(buf3));
+
+        XSetForeground(g.dpy, g.gc, c);
+        sprintf(buf6, "ball[x]: %d ball[y]: %d\n", g.shr->pos.x, g.shr->pos.y);
+        XDrawString(g.dpy, g.win, g.gc, 80, 200, buf6, strlen(buf6));
+        
     }
+
 }
 
 void physics(void)
 {
     //if (g.hasBall) {
 
-    if (((g.cpid == 0) && (g.shr->hasBall == CHILD))
-            || ((g.cpid != 0) && (g.shr->hasBall == PARENT))) {
+    if (((g.hasSpawned == 0 || g.cpid > 0) && (g.shr->hasBall == PARENT))
+    //if (((g.cpid == 0) && (g.shr->hasBall == CHILD))
+            || ((g.cpid == 0) && (g.shr->hasBall == CHILD))) {
 
         if (g.winner) {
             //g.winner = xtouch && ytouch;
@@ -621,25 +661,47 @@ void physics(void)
         // move ball
 
         // critical section 
-        g.shr->pos.x += 1 * g.shr->dir.x;
-        g.shr->pos.y += 1 * g.shr->dir.y;
 
         int xtouch = checkLRCollision();
         int ytouch = checkUDCollision();
         g.winner = xtouch && ytouch;
 
-        if (xtouch) {
+        if (xtouch && !g.winner) {
             // check if can go to other box
-            g.shr->dir.x *= -1;
-        } else if (ytouch) {
+            int b = checkExternalCollision((enum CollisionType)xtouch);
+            if (b == 1) {
+                printf("There was an external collision\n");
+                fflush(stdout);
+                g.shr->hasBall = (g.shr->hasBall == CHILD ? PARENT : CHILD);
+
+            } else {
+                g.shr->dir.x *= -1;
+            }
+
+        } else if (ytouch && !g.winner) {
             // check if can go to other box
             
+            int b = checkExternalCollision((enum CollisionType)ytouch);
+            if (b == 1) {
+                printf("There was a collision with the top/bottom wall");
+                fflush(stdout);
+                g.shr->hasBall = (g.shr->hasBall == CHILD ? PARENT : CHILD);
+            } else {
+                g.shr->dir.y *= -1;
+
+            }
+            
+            //b;
 
             // else 
-            g.shr->dir.y *= -1;
+
+
         } /*   else {
             // some events that occur in the middle of the window
         } */
+
+        g.shr->pos.x += 1 * g.shr->dir.x;
+        g.shr->pos.y += 1 * g.shr->dir.y;
 
         // end critical section
     }
@@ -648,16 +710,39 @@ void physics(void)
 int checkLRCollision(void) {
 
     //int epsilon = 0;
-    return (g.shr->pos.x > (g.xres-g.shr->dim.x) || g.shr->pos.x < 0);
+    int ct;
+    if (g.shr->pos.x > (g.xres-g.shr->dim.x)) {
+        ct = (int)RIGHT;
+    } else if (g.shr->pos.x < 0) {
+        ct = (int)LEFT;
+    } else {
+        ct = 0;
+    }
+
+    //return (g.shr->pos.x > (g.xres-g.shr->dim.x) || g.shr->pos.x < 0);
     //return (g.xres - (g.shr->pos.x + g.shr->dim.x) <= epsilon) || ((g.shr->pos.x) <= epsilon);
+    return ct;
 }
+
 
 int checkUDCollision(void) {
     
     //int epsilon = 0;
     //return (g.yres - (g.shr->pos.y + g.shr->dim.y) <= epsilon) || ((g.shr->pos.y) <= epsilon);
-    return (g.shr->pos.y > (g.yres-g.shr->dim.y) || g.shr->pos.y < 0);
+    int ct;
+
+    if (g.shr->pos.y > (g.yres-g.shr->dim.y)) {
+        ct = (int)BOTTOM;
+    } else if (g.shr->pos.y < 0) {
+        ct = (int)TOP;
+    } else {
+        ct = 0;
+    }
+    //return (g.shr->pos.y > (g.yres-g.shr->dim.y) || g.shr->pos.y < 0);
+    return ct;
 }
+
+
 
 int check_winner(void)
 {
@@ -701,15 +786,256 @@ void x11_setFont(unsigned int idx)
 }
 
 
-void getWindowCoords(int *x, int *y)
+void * getWindowCoords(void* arg)
 {
+    
     Window root = DefaultRootWindow(g.dpy);
     Window child;
+    XWindowAttributes xwa;
     
-    XGetWindowAttributes(g.dpy, root, &g.xwa);
-    XTranslateCoordinates(g.dpy, g.win, root, g.xwa.x, g.xwa.y, x, y, &child);
-#if DEBUG 
-    printf("wx: %d, wy: %d\n", wx, wy);
-#endif
+    XGetWindowAttributes(g.dpy, root, &xwa);
+    
+    if (g.hasSpawned == 0 || g.cpid > 0) {
+        // parent
+        printf("In getWindowCoords from parent\n");
+
+        while(g.runThread == 1) {
+
+            XTranslateCoordinates(g.dpy, g.win, root, xwa.x, xwa.y, 
+                &(g.shr->pWinPos.x), &(g.shr->pWinPos.y), &child);
+            usleep(1000);
+
+        }
+        
+
+
+    } else {
+        // parent
+
+        printf("In getWindowCoords from child\n");
+        while(g.runThread == 1) {
+
+            XTranslateCoordinates(g.dpy, g.win, root, xwa.x, xwa.y, 
+                &(g.shr->cWinPos.x), &(g.shr->cWinPos.y), &child);
+            usleep(1000);
+        }
+
+    }
+
+    return (void*)0;
+
+}
+
+int checkExternalCollision(enum CollisionType ct) {
+    struct Vec2 pass_pos, rec_pos, boxScreen_pos;
+    int slope;
+    char buf[100];
+
+    // assign vars depending on perspective
+   
+    if (g.hasSpawned == 0 || g.cpid > 0) {
+        // parent
+        sprintf(buf, "Checking collision in parent\n");
+        rec_pos = g.shr->cWinPos;
+        pass_pos = g.shr->pWinPos;
+    } else {
+        //child
+        sprintf(buf, "Checking collision in child\n");
+        pass_pos = g.shr->cWinPos;
+        rec_pos = g.shr->pWinPos;
+    }
+    
+    boxScreen_pos.x = pass_pos.x + g.shr->pos.x;
+    boxScreen_pos.y = pass_pos.y + g.shr->pos.y;
+    
+    printf("%s", buf);
+    printf("passer[x]: %d, passer[y]: %d\n", pass_pos.x, pass_pos.y);
+    printf("receiver[x]: %d, receiver[y]: %d\n", rec_pos.x, rec_pos.y);
+    //printf("Press return...\n");
+    fflush(stdout);
+    
+    //getchar();
+    int dist;
+    int y;
+    int xscreen, yscreen;
+    int isCollision1 = 0, isCollision2 = 0;
+    int begin, end;
+    // change in y over change in x
+    //
+
+    if (ct == LEFT) {
+
+        printf("**checking external collision from left wall**\n");
+        if (pass_pos.x >= rec_pos.x) {
+
+            /*
+            if ((rec_pos.y <= boxScreen_pos.y) && (g.shr->dir.y > 0))
+                return 0;
+            if ((rec_pos.y >= boxScreen_pos.y) && (g.shr->dir.y < 0))
+                return 0;
+                */
+
+            fflush(stdout);
+            dist = boxScreen_pos.x - (rec_pos.x + g.xres);
+
+
+            slope = (int)(g.shr->dir.y / abs(g.shr->dir.x));
+            printf("\tdist: %d  | slope: %d\n", dist, slope);
+            fflush(stdout);
+            y = dist * slope;
+            yscreen = boxScreen_pos.y + y;
+            xscreen = rec_pos.x + g.xres - g.shr->dim.x - 1;
+            if (xscreen > boxScreen_pos.x) return 0;
+            
+            printf("\ty: %d  | yscreen: %d\n", y, yscreen);
+            fflush(stdout);
+            begin = (yscreen >= rec_pos.y); 
+            end = (yscreen <= (rec_pos.y + g.yres - g.shr->dim.y));
+
+            isCollision1 = begin && end;
+            printf("\tbegin: %d && end: %d = %d\n", begin, end, isCollision1);
+            fflush(stdout);
+
+            if (isCollision1 == 1) {
+                printf("**\t\tCollision Found**\n");
+                fflush(stdout);
+                g.shr->pos.x = xscreen - rec_pos.x;
+                g.shr->pos.y = yscreen - rec_pos.y;
+            }
+
+        }
+
+    } else if (ct == RIGHT) {
+        // return if other box is too far left
+        //    printf("checking right external collision\n");
+
+        printf("**checking external collision from right wall**\n");
+        if (pass_pos.x <= rec_pos.x) {
+            /*
+            if ((rec_pos.y <= boxScreen_pos.y) && (g.shr->dir.y > 0))
+                return 0;
+            if ((rec_pos.y >= boxScreen_pos.y) && (g.shr->dir.y < 0))
+                return 0;
+                */
+
+            fflush(stdout);
+            dist = (rec_pos.x - boxScreen_pos.x - g.shr->dim.x);
+
+
+            slope = (int)(g.shr->dir.y / abs(g.shr->dir.x));
+            printf("\tdist: %d  | slope: %d\n", dist, slope);
+            fflush(stdout);
+            y = dist * slope;
+            yscreen = boxScreen_pos.y + y;
+
+            xscreen = rec_pos.x +1;
+            if (xscreen < boxScreen_pos.x) return 0;
+
+            printf("\ty: %d  | yscreen: %d\n", y, yscreen);
+            fflush(stdout);
+            begin = (yscreen >= rec_pos.y); 
+            end = (yscreen <= (rec_pos.y + g.yres - g.shr->dim.y));
+
+            isCollision1 = begin && end;
+            printf("\tbegin: %d && end: %d = %d\n", begin, end, isCollision1);
+            fflush(stdout);
+
+            if (isCollision1 == 1) {
+                printf("**\t\tCollision Found**\n");
+                fflush(stdout);
+                g.shr->pos.x = 1;
+                g.shr->pos.y = yscreen - rec_pos.y;
+            }
+
+        }
+
+    } else if (ct == TOP){
+
+        printf("**checking external collision from Top wall**\n");
+        if (pass_pos.y >= rec_pos.y) {
+            /*
+            if ((rec_pos.x <= boxScreen_pos.x) && (g.shr->dir.x > 0))
+                return 0;
+            if ((rec_pos.x >= boxScreen_pos.x) && (g.shr->dir.x < 0))
+                return 0;
+                */
+
+            fflush(stdout);
+            dist = boxScreen_pos.y - (rec_pos.y + g.yres);
+
+            slope = (int)(g.shr->dir.x / abs(g.shr->dir.y)); //dx/dy for this one
+            printf("\tdist: %d  | slope: %d\n", dist, slope);
+            fflush(stdout);
+            y = dist * slope;
+            yscreen = boxScreen_pos.x + y;
+
+            xscreen = rec_pos.y + g.yres - g.shr->dim.y;
+            if (xscreen > boxScreen_pos.y) return 0;
+
+            printf("\ty: %d  | yscreen: %d\n", y, yscreen);
+            fflush(stdout);
+            begin = (yscreen >= rec_pos.x); 
+            end = (yscreen <= (rec_pos.x + g.xres - g.shr->dim.x));
+
+            isCollision1 = begin && end;
+            printf("\tbegin: %d && end: %d = %d\n", begin, end, isCollision1);
+            fflush(stdout);
+
+            if (isCollision1 == 1) {
+                printf("**\t\tCollision Found going out the top**\n");
+                fflush(stdout);
+                g.shr->pos.x = yscreen - rec_pos.x;
+                g.shr->pos.y = xscreen - rec_pos.y - 1;
+            }
+
+        }
+
+
+    } else if (ct == BOTTOM){
+
+        if (pass_pos.y <= rec_pos.y) {
+            /*
+            if ((rec_pos.x <= boxScreen_pos.x) && (g.shr->dir.x > 0))
+                return 0;
+            if ((rec_pos.x >= boxScreen_pos.x) && (g.shr->dir.x < 0))
+                return 0;
+                */
+
+            fflush(stdout);
+            dist = rec_pos.y - boxScreen_pos.y - g.shr->dim.y;
+            slope = (int)(g.shr->dir.x / abs(g.shr->dir.y)); //dx/dy for this one
+            printf("\tdist: %d  | slope: %d\n", dist, slope);
+            fflush(stdout);
+            y = dist * slope;
+            yscreen = boxScreen_pos.x + y;
+
+            xscreen = rec_pos.y;
+            if (xscreen < boxScreen_pos.y) return 0;
+
+            printf("\ty: %d  | yscreen: %d\n", y, yscreen);
+            fflush(stdout);
+            begin = (yscreen >= rec_pos.x); 
+            end = (yscreen <= (rec_pos.x + g.xres - g.shr->dim.x));
+
+            isCollision1 = begin && end;
+            printf("\tbegin: %d && end: %d = %d\n", begin, end, isCollision1);
+            fflush(stdout);
+
+            if (isCollision1 == 1) {
+                printf("**\t\tCollision Found going out the top**\n");
+                fflush(stdout);
+                g.shr->pos.x = yscreen - rec_pos.x;
+                g.shr->pos.y = xscreen - rec_pos.y + 1; 
+            }
+
+        }
+
+    } else {
+
+        printf("checkExternCollision Param Error\n");
+
+    }
+
+    return isCollision1 || isCollision2;
 
 }
